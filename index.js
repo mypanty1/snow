6b9ccedefb925ab14cc163a327293eaa4c8d1bcc
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2023-03-24 15:40:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -133,3 +133,391 @@ Vue.component("SiteNodeInfo", {
   }
 });
 
+
+
+
+
+
+
+Vue.component('device-info',{
+  template:`<article class="device-info" :class="[addBorder&&'border-gray']" :style="neIsNotInstalled?'background-color:#eeeeee;':''">
+    <info-text-sec v-if="showLocation":text="networkElement?.region?.location" class="padding-left-right-unset margin-bottom-8px"/>
+    
+    <header class="device-info__header">
+      <div class="device-info__status" :class="'device-info__status--'+status" @click="updatePing" :title="sysUpTime">
+        <div :class="'ic-20 ic-'+(loading.ping?'loading rotating':'status')"></div>
+      </div>
+
+      <div @click="toNetworkElement" class="title display-flex align-items-center gap-4px">
+        <span class="title-ip">{{networkElementPrefix}} {{networkElement.ip}}</span>
+      </div>
+
+      <div v-if="!noMinimap&&neIsETH&&!neIsNotInstalled" class="device-info__minimap" @click="toNetworkElement">
+        <div class="device-info__ports--bad" :style="portsLine.bad"></div>
+        <div class="device-info__ports--busy" :style="portsLine.busy"></div>
+      </div>
+
+      <div v-if="noMinimap&&!showLink" class="margin-left-auto">
+        <i class="ic-20 ic-checkmark main-green"></i>
+      </div>
+
+      <slot name="link">
+        <button-sq v-if="showLink" @click="toNetworkElement" class="margin--10px">
+          <i class="ic-24 ic-right-link main-lilac"></i>
+        </button-sq>
+      </slot>
+    </header>
+
+    <div v-if="!hideEntrances" class="device-info__main" @click="toNetworkElement">
+      <div class="device-info__entrances">
+        <i class="ic-16 ic-entrance"></i>
+        <span class="device-info__entrance-dot"> • </span>
+        <template v-if="networkElementEntrances.length">
+          <div v-for="(entrance, index) of networkElementEntrances" class="device-info__entrance">
+            <span>{{entrance.number}}</span>
+            <span>{{(index + 1 < networkElementEntrances.length)?',':''}}</span>
+          </div>
+        </template>
+        <div v-else>Нет данных</div>
+      </div>
+    </div>
+
+    <div v-if="showNetworkElementAdminStatus" class="ne-admin-status">{{networkElementAdminStatus}}</div>
+    
+    <footer class="device-info__params" @click="toNetworkElement">
+      <info-value :label="networkElementLabel" value type="small" />
+      <info-subtitle v-if="sysName" :text="sysName"/>
+      
+      <div v-if="sysUpTime" class="width-100-100 display-flex align-items-center justify-content-end gap-4px">
+        <div class="font--13-500 tone-500">sysUpTime:</div>
+        <div class="font--13-500">{{sysUpTime}}</div>
+        <button-sq @click.stop="updateCmtsDeviceInfo" class="width-20px min-width-20px height-20px">
+          <span v-if="loadingSystem" class="ic-20 ic-loading rotating main-lilac"></span>
+          <span v-else class="ic-20 ic-refresh main-lilac"></span>
+        </button-sq>
+      </div>
+
+    </footer>
+    
+    <template v-if="showSessions&&neIsETH&&xRad_region_id">
+      <devider-line/>
+      
+      <title-main text="Активные сессии" :text2="!loading.sessions?sessionsOk:''" text2Class="main-green" :text3="!loading.sessions?sessionsBad:''" text3Class="main-orange" size="medium" @open="open.sessions=!open.sessions" class="padding-left-0" style="margin-top:-10px;" :style="!open.sessions?'margin-bottom:-10px;':''">
+        <button-sq :icon="loading.sessions?'loading rotating':'refresh'" type="medium" @click="get_device_session"/>
+      </title-main>
+
+      <div v-show="open.sessions">
+        <template v-for="(port_session,i) of sessions">
+          <devider-line v-if="i"/>
+          <port-info :port="port_session"/>
+        </template>
+      </div>
+    </template>
+
+    <slot name="ports">
+      <template v-for="(port,key) of ports">
+        <devider-line/>
+        <port-info-v1 v-bind="{port,key}" class="margin-left--8px width--webkit-fill-available" noSubs/>
+      </template>
+    </slot>
+  </article>`,
+  props:{
+    networkElement:{type:Object,required:true},
+    entrances:{type:Array},
+    hideEntrances:{type:Boolean,default:false},//обслуживаемые подъезды
+    showSessions:{type:Boolean,default:false},//кнопка сессий xRad
+    rack:{type:Object,default:()=>{}},
+    disabled:{type:Boolean,default:false},
+    ports:{type:Array,default:()=>[]},
+    showLocation:{type:Boolean,default:false},//адрес плошадки СЭ
+    showLink:{type:Boolean,default:false},//кнопка перехода на карточку СЭ из виджета
+    noMinimap:{type:Boolean,default:false},//линия утилизации
+    addBorder:{type:Boolean,default:false},//граница вокруг
+    showNetworkElementAdminStatus:{type:Boolean,default:false},
+    autoSysInfo:{type:Boolean,default:false},
+  },
+  data:()=>({
+    loading:{
+      ping:false,
+      dscv:false,
+      ports_lite:false,
+      sessions:false,
+      networkElementDeviceInfo:false,
+      deviceInfo:false,
+      networkElement:false,
+    },
+    response:{
+      ping:{
+        code:''
+      },
+      dscv:null,
+      ports_lite:null,
+      sessions:[],
+      networkElementDeviceInfo:null,
+      deviceInfo:null,
+      networkElement:null,
+    },
+    open:{
+      sessions:false,
+    },
+  }),
+  computed:{
+    loadingSystem(){return this.loading.deviceInfo||this.loading.networkElementDeviceInfo||this.loading.networkElement},
+    sysName(){return this.response.deviceInfo?.SysName||this.response.networkElementDeviceInfo?.sys_name||''},
+    sysUpTime(){return this.response.deviceInfo?.SysUptime||''},
+    isOnline(){
+      if(this.loading.ping){return};
+      return {200:true,400:false}[this.response.ping.code];
+    },
+    status(){
+      if(this.loading.ping||typeof this.isOnline!=='boolean'){return 'loading'};
+      return this.isOnline?'on':'off';
+    },
+    neIsETH(){return testByName.neIsETH(this.networkElement.name)},
+    neIsNotInstalled(){return testByName.neIsNotInstalled(this.networkElement.ne_status)},
+    networkElementPrefix(){return getNetworkElementPrefix(this.networkElement.name)},
+    portsLine(){
+      if(!this.response.ports_lite){return {}};
+      const {total,busy,bad}=this.response.ports_lite;
+      if(!total){return {}};
+      return {
+        busy:`width:${Math.round((busy/total)*100)}%;`,
+        bad:`width:${Math.round((bad/total)*100)}%;`,
+      };
+    },
+    networkElementEntrances(){return (this.entrances||[]).filter(({device_list})=>device_list.includes(this.networkElement.name))},
+    modelText(){
+      const {vendor,model,system_object_id,}=this.networkElement;
+      return getModelText(vendor,model,system_object_id);
+    },
+    networkElementAdminStatus(){return NIOSS_NE_ADM_STATUS[this.networkElement.ne_status]||''},
+    networkElementLabel(){
+      const {name}=this.networkElement;
+      const model_or_status=this.modelText||this.networkElementAdminStatus||'';
+      return name+(model_or_status?' • '+model_or_status:'')
+    },
+    xRad_region_id(){//TODO переделать ВЕ для фильтра по region_id коммутатора и площадки
+      return [
+        22,28,29,30,33,34,35,91,93,37,40,41,42,43,23,
+        24,46,77,52,53,54,55,56,57,58,59,25,2,3,12,14,
+        16,18,61,64,66,67,26,68,69,71,72,73,27,86,89,76
+      ].includes(this.networkElement.region.id);
+    },
+    sessions(){//only ports with account
+      return this.response.sessions.sort((a,b)=>a.number-b.number).filter(p=>p.account);
+    },
+    sessionsOk(){
+      return this.sessions.filter(s=>s?.session?.active).length;
+    },
+    sessionsBad(){
+      return this.sessions.filter(s=>!s?.session?.active).length;
+    },
+    isValidIcmpAttrs(){
+      const networkElement=this.response.networkElement||this.networkElement;
+      const {ip,region:{mr_id}}=networkElement;
+      return mr_id&&ip
+    },
+    isValidSnmpAttrs(){
+      const networkElement=this.response.networkElement||this.networkElement;
+      const {ip,region:{mr_id},snmp:{version,community}}=networkElement;
+      return mr_id&&ip&&version&&community
+    }
+  },
+  async created() {
+    if(this.neIsETH&&!this.neIsNotInstalled){
+      this.getPortsLite();
+    };
+    if(this.isValidIcmpAttrs){
+      this.getNetworkElementDeviceInfo();
+      await this.ping();
+      if(!this.isValidSnmpAttrs){
+        await this.getNetworkElement();
+      };
+      if(this.isOnline&&this.autoSysInfo){
+        this.updateCmtsDeviceInfo();
+      }
+    };
+  },
+  methods: {
+    updateCmtsDeviceInfo(){
+      this.getCmtsDeviceInfo(true);
+    },
+    async getNetworkElement(){
+      const {name}=this.networkElement;
+      this.loading.networkElement=true;
+      const cache=this.$cache.getItem(`device/${name}`);
+      if(cache){
+        this.response.networkElement=cache;
+      }else{
+        try{
+          const response=await httpGet(buildUrl('search_ma',{pattern:name,component:'device-info'},'/call/v1/search/'));
+          if(response.data){
+            this.$cache.setItem(`device/${name}`,response.data);
+            this.response.networkElement=response.data;
+          };
+        }catch(error){
+          console.warn('search_ma:device.error',error);
+        }
+      };
+      this.loading.networkElement=false;
+    },
+    async getCmtsDeviceInfo(update=false){
+      const networkElement=this.response.networkElement||this.networkElement;
+      const {ip,region:{mr_id},snmp:{version,community},name}=networkElement;
+      if(!ip||!mr_id||!version||!community){return};
+
+      const cache=this.$cache.getItem(`cmts_info_2/${name}`);
+      if(cache&&!update){
+        this.response.deviceInfo=cache;
+        return
+      };
+
+      this.loading.deviceInfo=true;
+      const response=await httpGet(buildUrl('cmts_info',objectToQuery({
+        ip,name,MR_ID:mr_id,IP_ADDRESS:ip,SNMP_VERSION:version,SNMP_COMMUNITY:community,
+        VENDOR:'HUAWEI', SYSTEM_OBJECT_ID:'.1.3.6.1.4.1.2011.2.249',ACT:'info',
+      }),'/call/hdm/')).catch(console.warn);
+      if(Array.isArray(response?.data)){
+        this.response.deviceInfo=response.data.reduce((data,row)=>{
+          const [key,[value]]=Object.entries(row)[0];
+          return Object.assign(data,{[key]:value});
+        },{});
+        this.$cache.setItem(`cmts_info_2/${name}`,this.response.deviceInfo,5);
+      };
+      this.loading.deviceInfo=false;
+    },
+    async getNetworkElementDeviceInfo(update=false){
+      const {name}=this.networkElement;
+      const cache=this.$cache.getItem(`get_dismantled_devices:installed/${name}`);
+      if(cache&&!update){
+        this.response.networkElementDeviceInfo=cache;
+        return
+      };
+
+      this.loading.networkElementDeviceInfo=true;
+      const response=await httpGet(buildUrl('get_dismantled_devices',{device_name:name},'/call/v1/device/')).catch(console.warn);
+      if(Array.isArray(response)){
+        this.response.networkElementDeviceInfo=response.find(device=>device.status_device=='INSTALLED_DEVICE');
+        this.$cache.setItem(`get_dismantled_devices:installed/${name}`,this.response.networkElementDeviceInfo);
+        this.$cache.setItem(`get_dismantled_devices/${name}`,response);
+      };
+      this.loading.networkElementDeviceInfo=false;
+    },
+    async updatePing(){//public
+      await this.ping();
+      if(this.isOnline){
+        this.updateCmtsDeviceInfo();
+      }
+    },
+    async ping(){
+      const {ip,region:{mr_id}}=this.networkElement;
+      if(!ip||!mr_id){return};
+      this.loading.ping=true;
+      try{
+        const response=await httpPost(`/call/hdm/device_ping`,{ip,mr_id,device:{MR_ID:mr_id,IP_ADDRESS:ip,SYSTEM_OBJECT_ID:null,VENDOR:null}}).catch(console.warn);
+        this.response.ping=response;
+      }catch(error){
+        console.warn('device_ping.error',error);
+      };
+      this.loading.ping=false;
+    },
+    async get_device_session(){//public
+      if(!this.neIsETH){return};
+      if(!this.showSessions){return};
+      if(!this.xRad_region_id){return};
+      if(this.loading.sessions){return};
+      this.response.sessions=[];
+      this.loading.sessions=true;
+      try{
+        const query=objectToQuery({name:this.networkElement.name,serverid:78});
+        const response=await httpGet(buildUrl('device_sessions',query,'/call/v1/device/')).catch(console.warn);
+        if(Array.isArray(response)){
+          this.response.sessions=response;
+        };
+      }catch(error){
+        console.warn('device_sessions.error',error);
+      };
+      this.loading.sessions=false;
+    },
+    async discovery(){//public
+      if(!this.networkElement.ip){return};
+      if(this.neIsNotInstalled){return};//СЭ не введен в эксплуатацию
+      const networkElement=this.response.networkElement||this.networkElement;
+      const {name,ip,region:{mr_id},system_object_id,vendor,snmp:{version,community}}=networkElement;
+      if(!version||!community){return};//если networkElement получен НЕ из массива по сайту(недостаточно данных)
+      this.loading.dscv=true;
+      try{
+        const response=await httpPost(buildUrl('dev_discovery',objectToQuery({ip,name}),'/call/hdm/'),{
+          ip,mr_id,
+          device:{
+            MR_ID:mr_id,DEVICE_NAME:name,IP_ADDRESS:ip,
+            SYSTEM_OBJECT_ID:system_object_id||'',VENDOR:vendor||'',
+            SNMP_VERSION:version,SNMP_COMMUNITY:community,
+          }
+        }).catch(console.warn);
+        this.response.dscv=response;
+        if(response?.code==200){
+          this.$cache.removeItem(`device/${name}`);
+          this.$cache.removeItem(`search_ma/${name}`);
+        };
+      }catch(error){
+        console.warn('dev_discovery.error',error);
+      };
+      this.loading.dscv=false;
+    },
+    async getPortsLite(){
+      const {name}=this.networkElement;
+      this.loading.ports_lite=true;
+      try{
+        const response=await httpGet(buildUrl('device_port_list_lite',{device:name,summary:'user'}));
+        if(response?.type!=='error'){
+          this.response.ports_lite=response;
+        };
+      }catch(error){
+        console.warn('device_port_list_lite.error',error);
+      };
+      this.loading.ports_lite=false;
+    },
+    toNetworkElement(){
+      if(this.disabled){return};
+      if(this.rack?.name){
+        this.$router.push({
+          name:'network-element-in-rack',
+          params:{
+            deviceProp:this.networkElement,
+            rackProp:this.rack,
+            device_id:this.networkElement.name,
+            rack_id:this.rack.name,
+          },
+        });
+      }else{
+        const prefix=getNetworkElementPrefix(this.networkElement.name);
+        if(prefix==='CMTS'){//CMTS_16KR_03383_1 site:9135155036813484532
+          this.$router.push({
+            name: "ds_device",
+            params: { name: this.networkElement.name},
+          });
+        }else{
+          this.$router.push({//direct route for support unmount ETH devices
+            name:"network-element",
+            params:{
+              device_id:this.networkElement.name,
+              deviceProp:this.networkElement,
+            },
+          });
+        }
+      };
+    },
+  },
+});
+
+
+
+
+
+
+
+
+
+
+
