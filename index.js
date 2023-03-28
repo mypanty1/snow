24fb39893077e419c01779194cd15295f5843d27
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2023-03-28 15:11:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -521,3 +521,439 @@ Vue.component('device-info',{
 
 
 
+
+Vue.component('SiteNetworkElements',{
+  template:`<CardBlock name="SiteNetworkElements" class="display-flex flex-direction-column gap-8px">
+    <template v-if="countRacksWithNetworkElements">
+      <title-main icon="server" text="ШДУ с оборудованием" text2Class="tone-500" :text2="countRacksWithNetworkElements||''"/>
+      <div v-for="({props:rackProps,networkElementsProps},rack_id) in racksProps" class="display-flex flex-direction-column gap-8px padding-left-right-16px">
+        <rack-box :key="rack_id" v-bind="rackProps">
+          <template v-for="({props,listeners},ne_id,i) in networkElementsProps">
+            <devider-line v-if="i"/>
+            <NetworkElementCard :key="ne_id" v-bind="props" v-on="listeners"/>
+          </template>
+        </rack-box>
+      </div>
+    </template>
+    <template v-if="countNetworkElementsNotInRack">
+      <title-main icon="warning" text="Место установки неизвестно" text2Class="tone-500" :text2="countNetworkElementsNotInRack||''"/>
+      <div v-for="({props,listeners},ne_id) in networkElementsProps" class="display-flex flex-direction-column gap-8px padding-left-right-16px">
+        <NetworkElementCard :key="ne_id" v-bind="props" v-on="listeners"/>
+      </div>
+    </template>
+  </CardBlock>`,
+  props:{
+    task_id:{type:String,required:true},
+    site_id:{type:String,required:true},
+  },
+  data:()=>({
+    auth_type:'',
+    serverid:'',
+  }),
+  async created(){
+    const {site_id,task_id}=this;
+    this.getRemedyWorkStages({task_id});
+    this.getSiteNodes({site_id});
+    this.getSiteEntrances({site_id});
+    await this.getSiteRacks({site_id});
+    await this.getSiteNetworkElements({site_id});
+    this.startIpoeDetection();
+  },
+  computed:{
+    ...mapGetters({
+      getSiteById:'site/getSiteById',
+      getEntrancesBySiteId:'site/getEntrancesBySiteId',
+      getRacksBySiteId:'site/getRacksBySiteId',
+      getNetworkElementsBySiteId:'site/getNetworkElementsBySiteId',
+      getRemedyWorkStagesResultById:'remedy/getRemedyWorkStagesResultById',
+      getRemedyWorkStagesLoadingById:'remedy/getRemedyWorkStagesLoadingById',
+      getTaskById:'remedy/getTaskById',
+    }),
+    task(){return this.getTaskById(atok(this.task_id,this.site_id))},
+    remedyWorkIsStarted(){return !!this.task?.started_at},
+    site(){return this.getSiteById(this.site_id)},
+    entrances(){return this.getEntrancesBySiteId(this.site_id)},
+    racks(){return this.getRacksBySiteId(this.site_id)},
+    networkElements(){return this.getNetworkElementsBySiteId(this.site_id)},
+    networkElementsFiltered(){
+      return select(this.networkElements,{
+        ne_name:testByName.neIsETH,
+        //node_name:testByName.nodeIsDu
+      })
+    },
+    stagesLoading(){return this.getRemedyWorkStagesLoadingById(this.task_id)},
+    stages(){return !this.stagesLoading?this.getRemedyWorkStagesResultById(this.task_id):null},
+    racksProps(){
+      const {task_id,remedyWorkIsStarted,serverid,isIpoe}=this;
+      return Object.values(this.racks).reduce((racks,rack)=>{
+        const {id,type,ne_in_rack}=rack;
+        if(type!="Антивандальный"){return racks};
+        racks[id]={
+          props:{
+            rack,
+          },
+          networkElementsProps:ne_in_rack?.reduce((networkElements,_name)=>{
+            const ne=Object.values(this.networkElementsFiltered).find(({ne_name})=>ne_name==_name);
+            if(!ne){return networkElements};
+            const {ne_id,site_id,ne_status,ne_name}=ne;
+            const isInstalled=testByName.neIsInstalled(ne_status);
+            const isAvailToSelectAsSource=isInstalled&&remedyWorkIsStarted;
+            networkElements[ne_id]={
+              props:{
+                ne_id,
+                site_id,
+                networkElementProps:ne,
+                showAdminStatus:true,
+                showSysDescr:true,
+                showSysName:true,
+                showServeEntrances:isInstalled,
+                showFlatsAbons:isInstalled,
+                replaced:neInStage(this.stages,ne_name)
+              },
+              listeners:{
+                ...isAvailToSelectAsSource?{
+                  click:()=>this.$router.push({
+                    name:"network-element-source",
+                    params:{
+                      task_id,
+                      site_id,
+                      source_ne_id:ne_id,
+                      networkElementProps:ne,
+                      serverid:isIpoe?serverid:''
+                    },
+                  })
+                }:null
+              }
+            };
+            return networkElements;
+          },{})
+        };
+        return racks
+      },{})
+    },
+    networkElementsProps(){
+      const {task_id,remedyWorkIsStarted,serverid,isIpoe}=this;
+      return Object.values(this.networkElementsFiltered).reduce((networkElements,ne)=>{
+        const {ne_id,site_id,rack_id,ne_status,ne_name}=ne;
+        const isInstalled=testByName.neIsInstalled(ne_status);
+        //const isAvailToStage=true||this.neIsAvailToStage(ne_name);
+        const isAvailToSelectAsSource=isInstalled&&remedyWorkIsStarted;
+        if(rack_id){return networkElements};
+        networkElements[ne_id]={
+          props:{
+            ne_id,
+            site_id,
+            networkElementProps:ne,
+            showBorder:true,
+            showAdminStatus:true,
+            showSysDescr:true,
+            showSysName:true,
+            showServeEntrances:isInstalled,
+            showFlatsAbons:isInstalled,
+            replaced:neInStage(this.stages,ne_name)
+          },
+          listeners:{
+            ...isAvailToSelectAsSource?{
+              click:()=>this.$router.push({
+                name:"network-element-source",
+                params:{
+                  task_id,
+                  site_id,
+                  source_ne_id:ne_id,
+                  networkElementProps:ne,
+                  serverid:isIpoe?serverid:''
+                },
+              })
+            }:null
+          }
+        };
+        return networkElements;
+      },{});
+    },
+    countRacksWithNetworkElements(){return Object.keys(this.racksProps).length},
+    countNetworkElementsNotInRack(){return Object.keys(this.networkElementsProps).length},
+    networkElementsInstalledNames(){
+      return Object.values(this.networkElementsFiltered).reduce((names,ne)=>{
+        const {ne_status,ne_name}=ne;
+        if(testByName.neIsInstalled(ne_status)){
+          names.push(ne_name);
+        };
+        return names;
+      },[]);
+    },
+    isIpoe(){return /IPoE/i.test(this.auth_type||'')}
+  },
+  methods:{
+    neIsAvailToStage(name){
+      if(!this.stages){return};
+      return !neInStage(this.stages,name);
+    },
+    ...mapActions({
+      getSiteNodes:'site/getSiteNodes',
+      getSiteEntrances:'site/getSiteEntrances',
+      getSiteRacks:'site/getSiteRacks',
+      getSiteNetworkElements:'site/getSiteNetworkElements',
+      getRemedyWorkStages:'remedy/getRemedyWorkStages',
+    }),
+    async startIpoeDetection(){
+      for(const name of this.networkElementsInstalledNames){
+        const ports=await this.device_port_list(name);
+        if(!ports?.length){continue};
+        const subscribers=ports.map(({subscriber_list,name,flat,last_mac})=>{
+          const account=subscriber_list.find(({account})=>account)?.account;
+          //if(!account){console.warn({name,flat,account,last_mac:last_mac?last_mac.value:null,last_at:last_mac?last_mac.last_at:null})}
+          return account
+        }).filter(v=>v);
+        if(!subscribers.length){continue};
+
+        for(const account of subscribers){
+          const account_data=await this.search_ma(account);
+          if(!account_data){continue};
+
+          const agreements=account_data?.lbsv?.data?.agreements||account_data?.lbsv?.data?.[0]?.agreements||[];
+          const agreement=agreements?.find(({account:_account})=>_account==account);
+          if(!agreement?.services?.internet?.vgroups){continue};
+
+          const service_internet_active=agreement.services.internet.vgroups.find(({statusname})=>statusname=='Активна');
+          if(!service_internet_active){continue};
+
+          const {serverid,vgid,login}=service_internet_active;
+          const auth_type=await this.get_auth_type({serverid,vgid,login});
+          if(!auth_type){continue};
+          this.auth_type=auth_type;
+          this.serverid=serverid;
+          break;
+        };
+        if(this.auth_type){break};
+      };
+    },
+    async device_port_list(name){
+      try{
+        const cache=this.$cache.getItem(`device_port_list/${name}`);
+        if(cache){
+          return cache.response||[];
+        }else{
+          const response = await httpGet(buildUrl('device_port_list', {device:name}));
+          if(!response?.length){
+            return
+          };
+          this.$cache.setItem(`device_port_list/${name}`,{date:new Date(),response},60);
+          for(const port of response){//т.к структура идентичная сваливаем порты в кэш
+            this.$cache.setItem(`port/PORT-${name}/${port.snmp_number}`,port);
+          };
+          return response;
+        };
+      }catch(error){
+        console.warn("device_port_list.error",error)
+      };
+      return
+    },
+    async search_ma(account){
+      try{
+        const response=await httpGet(buildUrl('search_ma',{pattern:account},'/call/v1/search/'));
+        if(response?.data){
+          return response.data;
+        };
+      }catch(error){
+        console.warn("search_ma:account.error",error)
+      };
+      return
+    },
+    async get_auth_type({serverid,vgid,login}){
+      try{
+        const response=await httpGet(buildUrl('get_auth_type',{serverid,vgid,login},'/call/aaa/'));
+        if(response?.code==200&&response?.data?.[0].auth_type){
+          return response.data[0].auth_type;
+        }
+      }catch(error){
+        console.warn("get_auth_type.error",error)
+      };
+      return
+    }
+  },
+});
+
+Vue.component('SiteNetworkElementsPlanned',{
+  template:`<CardBlock name="SiteNetworkElementsPlanned">
+    <title-main icon="server" text="Заменить на новый коммутатор" text2Class="tone-500" :text2="countNetworkElementsPlanned||''" @open="opened=!opened"/>
+    <div v-show="opened" class="display-flex flex-direction-column gap-8px padding-left-right-16px-">
+      <message-el v-if="!notSelectableTargetNetworkElements" type="info" text="Выберите новый коммутатор" box @click="unselect"/>
+      
+      <template v-if="countRacksWithNetworkElements">
+        <title-main icon="server" text="ШДУ с оборудованием" text2Class="tone-500" :text2="countRacksWithNetworkElements||''"/>
+        <div v-for="({props:rackProps,networkElementsProps},rack_id) in racksProps" class="display-flex flex-direction-column gap-8px padding-left-right-16px">
+          <rack-box :key="rack_id" v-bind="rackProps">
+            <template v-for="({props,listeners},ne_id,i) in networkElementsProps">
+              <devider-line v-if="i"/>
+              <NetworkElementCard :key="ne_id" v-bind="props" v-on="listeners"/>
+            </template>
+          </rack-box>
+        </div>
+      </template>
+      <template v-if="countNetworkElementsNotInRack">
+        <title-main icon="warning" text="Место установки неизвестно" text2Class="tone-500" :text2="countNetworkElementsNotInRack||''"/>
+        <div v-for="({props,listeners},ne_id) in networkElementsProps" class="display-flex flex-direction-column gap-8px padding-left-right-16px">
+          <NetworkElementCard :key="ne_id" v-bind="props" v-on="listeners"/>
+        </div>
+      </template>
+
+      <div class="display-flex padding-left-right-16px">
+        <button-main v-if="!notSelectableTargetNetworkElements" label="Выбрать новый коммутатор" @click="goToSelected" :disabled="!selected_ne_id||!countNetworkElementsPlanned" buttonStyle="contained" size="full"/>
+      </div>
+    </div>
+    <SelectedNetworkElementPlannedModal v-if="!notSelectableTargetNetworkElements&&selected_ne_id" v-bind="{target_ne_id:selected_ne_id,source_ne_id,task_id,next_stage_id,serverid}" ref="SelectedNetworkElementPlannedModal"/>
+  </CardBlock>`,
+  props:{
+    task_id:{type:String,required:true},
+    site_id:{type:String,required:true},
+    source_ne_id:{type:String},//коммутатор источник привязок
+    next_ne_id:{type:String,default:''},//автовыделенный коммутатор
+    serverid:{type:[Number,String],default:''},
+    notSelectableTargetNetworkElements:{type:Boolean,default:false},
+  },
+  data:()=>({
+    opened:true,
+    selected_ne_id:null
+  }),
+  async created(){
+    this.selected_ne_id=this.next_ne_id||this.selected_ne_id;//9154819735513194600
+    const {site_id,stages,task_id}=this;
+    if(!stages?.length){
+      this.getRemedyWorkStages({task_id});
+    };
+    this.getSiteNodes({site_id});
+    this.getSiteEntrances({site_id});
+    await this.getSiteRacks({site_id});
+    this.getSiteNetworkElements({site_id});
+    this.getSiteNetworkElementsPlanned({site_id});
+  },
+  computed:{
+    ...mapGetters({
+      getSiteById:'site/getSiteById',
+      getEntrancesBySiteId:'site/getEntrancesBySiteId',
+      getRacksBySiteId:'site/getRacksBySiteId',
+      getNetworkElementsPlannedBySiteId:'site/getNetworkElementsPlannedBySiteId',
+      getRemedyWorkStagesResultById:'remedy/getRemedyWorkStagesResultById',
+      getRemedyWorkStagesLoadingById:'remedy/getRemedyWorkStagesLoadingById',
+    }),
+    site(){return this.getSiteById(this.site_id)},
+    entrances(){return this.getEntrancesBySiteId(this.site_id)},
+    racks(){return this.getRacksBySiteId(this.site_id)},
+    networkElements(){return this.getNetworkElementsPlannedBySiteId(this.site_id)},
+    networkElementsFiltered(){
+      return select(this.networkElements,{
+        ne_name:testByName.neIsETH,
+        //node_name:testByName.nodeIsDu,
+        ne_status:testByName.statusIsP
+      })
+    },
+    stagesLoading(){return this.getRemedyWorkStagesLoadingById(this.task_id)},
+    stages(){return !this.stagesLoading?this.getRemedyWorkStagesResultById(this.task_id):null},
+    next_stage_id(){return Array.isArray(this.stages)?this.stages.length:-1},
+    networkElementsProps(){
+      return Object.values(this.networkElementsFiltered).reduce((networkElements,ne)=>{
+        const {ne_id,site_id,rack_id,ne_name}=ne;
+        const isAvailToSelectAsTarget=this.neIsAvailToStage(ne_name)&&!this.notSelectableTargetNetworkElements;
+        if(rack_id){return networkElements};
+        networkElements[ne_id]={
+          props:{
+            ne_id,
+            site_id,
+            networkElementProps:ne,
+            showBorder:true,
+            showAdminStatus:true,
+            showSysDescr:true,
+            showSysName:true,
+            selected:this.selected_ne_id==ne_id,
+            showServeEntrances:true,
+            showFlatsAbons:true,
+          },
+          listeners:{
+            ...isAvailToSelectAsTarget?{
+              click:()=>{this.selected_ne_id=ne_id}
+            }:null
+          }
+        };
+        return networkElements
+      },{});
+    },
+    racksProps(){
+      return Object.values(this.racks).reduce((racks,rack)=>{
+        const {id,type,ne_in_rack}=rack;
+        if(type!="Антивандальный"){return racks};
+        racks[id]={
+          props:{
+            rack,
+          },
+          networkElementsProps:ne_in_rack?.reduce((networkElements,_name)=>{
+            const ne=Object.values(this.networkElementsFiltered).find(({ne_name})=>ne_name==_name);
+            if(!ne){return networkElements};
+            const {ne_id,site_id,ne_name}=ne;
+            const isAvailToSelectAsTarget=this.neIsAvailToStage(ne_name)&&!this.notSelectableTargetNetworkElements;
+            networkElements[ne_id]={
+              props:{
+                ne_id,
+                site_id,
+                networkElementProps:ne,
+                showAdminStatus:true,
+                showSysDescr:true,
+                showSysName:true,
+                selected:this.selected_ne_id==ne_id,
+                showServeEntrances:true,
+                showFlatsAbons:true,
+              },
+              listeners:{
+                ...isAvailToSelectAsTarget?{
+                  click:()=>{this.selected_ne_id=ne_id}
+                }:null
+              }
+            };
+            return networkElements;
+          },{})
+        };
+        return racks
+      },{})
+    },
+    countNetworkElementsPlanned(){return Object.keys(this.networkElementsFiltered).length},
+    countRacksWithNetworkElements(){return Object.keys(this.racksProps).length},
+    countNetworkElementsNotInRack(){return Object.keys(this.networkElementsProps).length},
+  },
+  methods:{
+    neIsAvailToStage(name){
+      if(!this.stages){return};
+      return !neInStage(this.stages,name);
+    },
+    ...mapActions({
+      getSiteNodes:'site/getSiteNodes',
+      getSiteEntrances:'site/getSiteEntrances',
+      getSiteRacks:'site/getSiteRacks',
+      getSiteNetworkElements:'site/getSiteNetworkElements',
+      getSiteNetworkElementsPlanned:'site/getSiteNetworkElementsPlanned',
+      getRemedyWorkStages:'remedy/getRemedyWorkStages',
+    }),
+    unselect(){this.selected_ne_id=''},
+    goToSelected(){
+      if(!this.selected_ne_id){return};
+      if(!this.$refs.SelectedNetworkElementPlannedModal){return};
+      this.$refs.SelectedNetworkElementPlannedModal.open();
+    },
+  },
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
