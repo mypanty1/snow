cedacceabada73a833c5cbacc003476f65ac4360
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2023-06-20 19:07:25 +0700

    Update SessionItem.js

--- SessionItem.js
+++ SessionItem.js
@@ -1,4 +1,4 @@
-//fix time zone
+//fix time zone and ip
 Vue.component('SessionItem',{
   template:`<section name="SessionItem">
     <title-main v-bind="titleProps" textClass="font--13-500" class="margin-top-bottom--8px"/>
@@ -19,10 +19,10 @@ Vue.component('SessionItem',{
           <info-value v-if="service_info" label="Сервис" :value="service_info" withLine class="padding-unset"/>
           <info-value v-if="inner_vlan" label="C-Vlan" :value="inner_vlan" withLine class="padding-unset"/>
           <info-value v-if="outer_vlan" label="S-Vlan" :value="outer_vlan" withLine class="padding-unset"/>
-          <info-value v-if="port" label="Opt.82 Порт" :value="agent_circuit_id" withLine class="padding-unset"/>
-          <info-value v-if="device" label="Opt.82 Коммутатор" :value="agent_remote_id" withLine class="padding-unset"/>
+          <info-value v-if="agent_circuit_id" label="Opt.82 Порт" :value="agent_circuit_id" withLine class="padding-unset"/>
+          <info-value v-if="agent_remote_id" label="Opt.82 Коммутатор" :value="agent_remote_id" withLine class="padding-unset"/>
           <info-text-sec v-if="deviceMacVendor" :text="deviceMacVendor" class="padding-unset text-align-right"/>
-          <info-value v-if="nas" label="BRAS" :value="nas" withLine class="padding-unset" data-ic-test="session_nas"/>
+          <info-value v-if="brasIpOrHostName" label="BRAS" :value="brasIpOrHostName" withLine class="padding-unset" data-ic-test="session_nas"/>
         </div>
         <div class="display-flex justify-content-space-between gap-4px margin-bottom-8px">
           <button-main @click="$refs.SessionHistoryModal.open()" button-style="outlined" :disabled="false" icon="history" label="История" loading-text="" size="large" data-ic-test="session_history_btn" />
@@ -57,8 +57,8 @@ Vue.component('SessionItem',{
     'mac'(mac){
       if(mac&&this.macIsValid){this.getMacVendorLookup(mac)};
     },
-    'deviceMac'(deviceMac){
-      if(deviceMac){this.getMacVendorLookup(deviceMac)};
+    'remote_id_mac'(remote_id_mac){
+      if(remote_id_mac){this.getMacVendorLookup(remote_id_mac)};
     },
   },
   created(){ 
@@ -76,19 +76,19 @@ Vue.component('SessionItem',{
     session(){return this.resps.get_online_sessions?.data?.[0]||this.resps.get_online_sessions},
     isError(){return this.session?.isError},
     errorText(){return this.session?.message||'Error: unknown'},
-    device(){return this.session?.device||''},
-    deviceStr(){return `${this.device||''}`},
-    deviceMac(){return ((this.deviceStr.match(/^[a-f0-9]{12}$/gi)?.[0]||'').match(/.{4}/gi)||[]).join('.')},
+    remote_id(){return this.session?.remote_id||this.session?.device||''},
+    remote_id_str(){return `${this.remote_id||''}`},
+    remote_id_mac(){return ((this.remote_id_str.match(/^[a-f0-9]{12}$/gi)?.[0]||'').match(/.{4}/gi)||[]).join('.')},
     agent_remote_id(){
-      const {deviceStr,deviceMac}=this;
-      if(deviceMac){//30150037478 - default format
-        return deviceMac;
+      const {remote_id_str,remote_id_mac}=this;
+      if(remote_id_mac){//30150037478 - default format
+        return remote_id_mac;
       };
-      const isNotHex=/\W/i.test(deviceStr);
+      const isNotHex=/\W/i.test(remote_id_str);
       if(isNotHex){//10702046999 - ascii format
-        return deviceStr
+        return remote_id_str
       };
-      return (deviceStr.match(/.{2}/gi)||[]).map(b=>{
+      return (remote_id_str.match(/.{2}/gi)||[]).map(b=>{
         b=b.padStart(2,0);
         try{//60910533888 - custom format
           return decodeURIComponent('%'+b);
@@ -97,29 +97,37 @@ Vue.component('SessionItem',{
         };
       }).join('');
     },
-    ip(){return this.session?.ip||''},
+    ip(){return this.parseIp(this.session?.ip||'')},
     mac(){return this.session?.mac||''},
-    nas(){return this.session?.nas||''},
-    port(){return this.session?.port||''},
-    agent_circuit_id(){return `${this.port||''}`},//40206334997
+    brasIpOrHostName(){return this.parseIp(this.session?.nas||'')},
+    agent_circuit_id(){return `${this.session?.port||''}`},//40206334997
     sessionid(){return this.session?.sessionid||''},
     inner_vlan(){return this.session?.inner_vlan||''},
     outer_vlan(){return this.session?.outer_vlan||''},
     service_info(){return this.session?.service_info||''},
     start(){return this.session?.start||''},
-    timeZone(){return this.session?.timeZone||''},
     startLocal(){
-      const {start,timeZone}=this;
-      if(!start||!timeZone){return};
+      const {start}=this;
+      if(!start){return};
       const date=new Date(start);
-      date.setHours(date.getHours()+(timeZone-3));
+      if(date=='Invalid Date'){return start};
+      const offset=new Date().getTimezoneOffset()/-60;
+      date.setHours(date.getHours()-3+offset);
       return Date.prototype.toDateTimeString?date.toDateTimeString():date.toLocaleString();
     },
     macIsValid(){return this.mac&&this.mac!=='0000.0000.0000'},
     macVendor(){return this.ouis[this.mac]},
-    deviceMacVendor(){return this.ouis[this.deviceMac]},
+    deviceMacVendor(){return this.ouis[this.remote_id_mac]},
   },
   methods:{
+    parseIp(value=''){
+      if(/[.]/.test(`${value}`)){return value};
+      const int=parseInt(value);
+      if(!int){return value};
+      const octs=int.toString(16).padStart(8,0).match(/[0-9a-f]{2}/gi);
+      if(!octs||octs?.length!==4){return value};
+      return octs.map(h=>parseInt(h,16)).join('.');
+    },
     async get_online_sessions(){
       if(this.loads.get_online_sessions){return};
       this.resps.get_online_sessions=null;
