ee1622f4590ff3eae0ca97f1640296320f5c6e87
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2023-07-01 10:17:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@
 document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/snow/FavEditOrRemoveModal.js',type:'text/javascript'}));
 document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/snow/FavBtnLinkBlock_FavBtnDel.js',type:'text/javascript'}));
 document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/snow/SessionItem.js',type:'text/javascript'}));
-
+document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/snow/events-item.js',type:'text/javascript'}));
 
 //hide deprecated
 Vue.component("account-block-history-dis", {
