4c94ba5065a78be33b6ee30bd09b97fc0b5b442c
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2023-07-18 17:15:54 +0700

    Update AppHeader.js

--- AppHeader.js
+++ AppHeader.js
@@ -1,6 +1,6 @@
 Vue.component('app-header',{
   template:`<div class="display-contents">
-    <AppHeader3 v-if="isApp||username=='mypanty1'" v-on="$listeners"/>
+    <AppHeader3 v-if="true||isApp||username=='mypanty1'" v-on="$listeners"/>
     <AppHeader2 v-else v-on="$listeners"/>
   </div>`,
   computed:{
