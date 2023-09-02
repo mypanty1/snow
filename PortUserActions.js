6c595ff17e2ddfae1830d44ee46c2385d01d3dc5
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2023-09-02 11:41:59 +0700

    Update PortUserActions.js

--- PortUserActions.js
+++ PortUserActions.js
@@ -80,7 +80,7 @@ Vue.component("PortUserActions",{
 
 Vue.component("PortActionMac", {
   template:`<section name="PortActionMac">
-    <link-block icon="mac" :text="'MAC-адрес__'+testId" @block-click="loadMacs" :disabled="disabledBtn" :loading="loading" actionIcon="down" data-ic-test="load_mac_btn"/>
+    <link-block icon="mac" text="MAC-адрес_2" @block-click="loadMacs" :disabled="disabledBtn" :loading="loading" actionIcon="down" data-ic-test="load_mac_btn"/>
     <template v-if="!loading&&rows.length">
       <PortEntitiesByMac v-for="({text,mac},key) of rows" :key="key" v-bind="{text,mac}" :oui="ouis[mac]" :mr_id="networkElement.region.mr_id" :region_id="networkElement.region.id" :noSession="rows.length>2"/>
     </template>
@@ -120,7 +120,6 @@ Vue.component("PortActionMac", {
     disabledBtn() {
       return this.disabled || this.loading || this.loading_clear;
     },
-    testId(){return randcode(10)},
   },
   methods: {
     async parse(rows){
