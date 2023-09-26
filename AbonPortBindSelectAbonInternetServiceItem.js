3090341c95aac5219e37b2a8a91284c29a16c541
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2023-09-26 14:28:49 +0700

    Update AbonPortBindSelectAbonInternetServiceItem.js

--- AbonPortBindSelectAbonInternetServiceItem.js
+++ AbonPortBindSelectAbonInternetServiceItem.js
@@ -1,3 +1,163 @@
+//fix cpe undefined
+Vue.component('AbonPortBindModal',{
+  template:`<div name="AbonPortBindModal">
+    <modal-container-custom ref="modal" :disabled="macsLoading||setBindLoading" header :footer="false" @open="onOpen" @close="onClose" :wrapperStyle="{'min-height':'auto','margin-top':'4px'}">
+      <div class="padding-left-right-16px display-flex flex-direction-column gap-16px">
+        <div class="display-flex flex-direction-column">
+          <div class="font--15-600 text-align-center">Привязать ЛС</div>
+          <div class="font--13-500 tone-500 text-align-center">{{subtitle}}</div>
+        </div>
+        
+        <div class="display-flex flex-direction-column gap-8px">
+          <AbonPortBindSearchAbon ref="AbonPortBindSearchAbon" @onAccountFind="account=$event" @onVgSelect="vg=$event" @onMrId="mrId=$event"/>
+          
+          <template v-if="vg&&mac">
+            <title-main :text="cpesLoading?'поиск CPE...':cpeTitle" textSize="medium" :opened="openBindCPE" @block-click="openBindCPE=!openBindCPE" class="padding-unset">
+              <button-sq @click.stop="getCpes" :icon="cpesLoading?'loading rotating':'refresh'"/>
+            </title-main>
+            <template v-if="openBindCPE">
+              <service-activation-cpe ref="service_activation_cpe" v-bind="{account,service:vg,cpes,macs,port:{device:networkElement}}"/>
+              <button-main :disabled="cpesLoading" @click="$refs.service_activation_cpe.changeEquipment()" label="Привязать CPE" buttonStyle="contained" size="full"/>
+            </template>
+          </template>
+
+          <template v-if="vg?.type_of_bind">
+            <AbonPortBindForm v-bind="{networkElement,port,account,vg,macs,macsLoading,disabled:macsLoading||setBindLoading||vgNotActive131,loading:macsLoading||setBindLoading}" @loading="setBindLoading=$event" @result="setBindResult=$event" @mac="mac=$event" @callParent="doCall"/>
+            <AbonPortRefree v-bind="{setBindResult,networkElement,port,account,vg,disabled:macsLoading||setBindLoading,loading:macsLoading||setBindLoading}" @loading="setBindLoading=$event"/>
+          </template>
+          <template v-else-if="vg">
+            <message-el text="Привязка этой УЗ не предусмотрена" type="info" box/>
+          </template>
+        </div>
+        <div class="display-flex justify-content-space-around">
+          <button-main label="Закрыть" @click="close" :disabled="macsLoading||setBindLoading" buttonStyle="outlined" size="medium"/>
+        </div>
+      </div>
+    </modal-container-custom>
+  </div>`,
+  props:{
+    port:{type:Object,required:true},
+    networkElement:{type:Object,required:true},
+  },
+  data:()=>({
+    account:'',
+    vg:null,
+    mrId:0,
+    macsLoading:false,
+    macs:[],
+    macsErrorText:'',
+    setBindLoading:false,
+    setBindResult:null,
+    mac:'',
+    cpesLoading:false,
+    cpes:[],
+    openBindCPE:false,
+  }),
+  watch:{
+    'typeOfBind'(typeOfBind){
+      if(typeOfBind){
+        this.getMacsFirst()
+      };
+    },
+    'mrId'(mrId){
+      if(mrId){
+        this.getCpes()
+      }
+    },
+    'mac'(mac){
+      if(mac){
+        this.getCpes()
+      }
+    },
+    'setBindResult'(setBindResult){
+      if(setBindResult?.code==200||setBindResult?.InfoMessage||setBindResult?.Data){
+        if(this.$refs.service_activation_cpe){this.$refs.service_activation_cpe.cpeRegistre()};
+      }
+    }
+  },
+  computed:{
+    subtitle(){return `${this.networkElement?.ip} порт ${this.port?.snmp_name||this.port?.number}`},
+    typeOfBind(){return this.vg?.type_of_bind},
+    vgNotActive131(){
+      const {serverid,available_for_activation}=this.vg||{};
+      return serverid==131&&available_for_activation;
+    },
+    hasCpe(){return Boolean(this.cpes?.[0])},
+    cpeTitle(){return `добавить CPE ${this.cpes?.[0]?.model||''}`},
+  },
+  methods:{
+    open(){//public
+      this.$refs.modal.open();
+    },
+    close(){//public
+      this.$refs.modal.close();
+    },
+    onOpen(){
+      this.getMacsFirst();
+    },
+    onClose(){
+      this.clear();
+    },
+    clear(){
+      this.macs=[];
+      this.macsLoading=false;
+      this.macsErrorText='';
+      this.$refs.AbonPortBindSearchAbon?.clear();
+      this.account='';
+      this.vg=null;
+      this.mrId=0;
+      this.setBindLoading=false;
+      this.setBindResult=null;
+      this.mac='';
+    },
+    doCall(event){
+      if(!Array.isArray(event)){return}
+      const [methodName,...attrs]=event;
+      if(typeof this?.[methodName]=='function'){
+        this[methodName](...attrs);
+      };
+    },
+    getMacsFirst(){
+      //if(![2,5,7,9,10].includes(parseInt(this.typeOfBind))){return};
+      if(this.macs.length){return};
+      this.getMacs();
+    },
+    async getMacs(){
+      this.macsLoading=!0;
+      this.macs=[];
+      this.macsErrorText='';
+      try{
+        const response=await httpGet(buildUrl('port_mac_show',{type:'array',...new DNM.DevicePortParams(this.networkElement,this.port)},"/call/hdm/"));
+        if(Array.isArray(response?.text)){
+          this.macs=response.text/*.reduce((macs,row)=>{
+            const mac=((row.match(/\S{12,17}/gi)||[''])[0].replaceAll(/[^0-9A-Fa-fx]/gi,'').match(/[0-9A-Fa-fx]{12}/gi)||[''])[0];
+            if(mac){macs.push(mac)};
+            return macs;
+          },[]);*/
+        };
+      }catch(error){
+        console.warn("port_mac_show.error",error);
+      }
+      this.macsLoading=!1;
+    },
+    async getCpes(){
+      const {macs,mac,mrId}=this;
+      const _mac=mac||macs[0];
+      if(!_mac||!mrId){return};
+      this.cpesLoading=!0;
+      this.cpes=[];
+      const cacheKey=`routersByMac-${mrId}-${_mac}`;
+      try{
+        const response=this.$cache.getItem(cacheKey)||await httpGet(buildUrl('cpe_registre',{mac:_mac,mr_id:mrId,mr:mrId,mrId},'/call/axiros/')/*,{mac:_mac,mr_id:mrId,mr:mrId,mrId}*/);
+        this.$cache.setItem(cacheKey,this.cpes=(Array.isArray(response)?response:[]).map(cpe=>({...cpe,mac:_mac,mr_id:mrId,mr:mrId,mrId})));
+      }catch(error){
+        console.warn('cpe_registre.error', error);
+      };
+      this.cpesLoading=!1;
+    },
+  },
+});
+
 //add activateIsOk
 Vue.component('AbonPortBindSelectAbonInternetServiceItem',{
   template:`<div name="AbonPortBindSelectAbonInternetServiceItem">
